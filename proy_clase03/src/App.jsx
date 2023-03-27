8740e34c9647dcb1c9c7f82cb861933719662397
Catalina Hernandez <cataxto@gmail.com>
2023-03-27 18:19:35 -0500

    Clase 5 Limpiando Card en App.jsx

--- proy_clase03/src/App.jsx
+++ proy_clase03/src/App.jsx
@@ -1,6 +1,5 @@
 
 import './App.css' 
-import Card from './components/Card'
 import OtroClassComponent from './components/ClassComponent' /*Puedo usar otros nombres, el ya sabe que exportara y usara*/
 import FuncComponent  from './components/FuncComponent'
 import Home from './components/Home'
@@ -23,9 +22,9 @@ function App() {
 
       
       <h1> Titulo de Card: Elemento Clase 4</h1>
-      <Card elemento = {elementos[0]} /> {/*Elemento 1  */}
-      <Card elemento = {elementos[1]} /> {/*Elemento 1  */}
-      <Card elemento = {elementos[2]} /> {/*Elemento 1  */}
+   {/*    <Card elemento = {elementos[0]} /> */}{/*Elemento 1  */}
+    {/*  <Card elemento = {elementos[1]} /> */}{/*Elemento 1  */}
+      
 
       <h1> Titulo de Card: Elemento Clase 5</h1>
       <Home/>

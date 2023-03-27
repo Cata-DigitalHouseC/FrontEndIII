bec522e23a87b37b0a0fb4d720e93e89b0e12dbb
Catalina Hernandez <cataxto@gmail.com>
2023-03-26 21:20:45 -0500

    Incorporando componentes y props al proyecto

--- proy_clase03/src/App.jsx
+++ proy_clase03/src/App.jsx
@@ -1,18 +1,43 @@
 
 import './App.css' 
+import Card from './components/Card'
+import ClassComponent from './components/ClassComponent'
+import FuncComponent  from './components/FuncComponent'
 
 function App() {
+
+  let a ='Elemento 0'
+  let elementos =[1,2,3]
+
+
   return (
+/*CON COMPONENTES*/
     <>
+      <ClassComponent />
+      <FuncComponent />
+      <FuncComponent />
+
+
+      
+      <h1> Titulo de Card: Elemento </h1>
+      <Card elemento = {elementos[0]} /> {/*Elemento 1  */}
+      <Card elemento = {elementos[1]} /> {/*Elemento 1  */}
+      <Card elemento = {elementos[2]} /> {/*Elemento 1  */}
+    </>
+
+
+
+/*     <>   SIN COMPONENTES
     <h1> Hola Mundo</h1>
     <h2>titulo</h2>
     <ul>
+      <li>{a}</li>
       <li>Item 1</li>
       <li>Item 2</li>
       <li>Item 3</li>
       <li>Item 4</li>
     </ul>
-    </>
+    </> */
   )
 }
 
---
+++ proy_clase03/src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+const Card = (props) => {
+
+  console.log(props)  
+  return (
+    <>
+        <h3> Titulo card: elemento # {props.elemento}</h3>
+    </>
+  )
+}
+
+export default Card

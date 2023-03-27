087c3a8e65ea86cae36a665f1a4b4cc5077beb87
Catalina Hernandez <cataxto@gmail.com>
2023-03-26 20:19:45 -0500

    Creando proyecto React con Vite

---
+++ proy_clase03/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css' 
+
+function App() {
+  return (
+    <>
+    <h1> Hola Mundo</h1>
+    <h2>titulo</h2>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
